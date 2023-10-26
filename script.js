22cbd113672f4b9890e1279070ea5efaf6ac3ffb
Naveen-Raj-Sivasamy <naveenrajsivasamy@gmail.com>
2023-10-26 09:43:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Introduction to Objects
+
+
+var personDetails = {
+
+    Name: "Naveen Raj Sivasamy",
+    Age: 29,
+    Gender: "Male",
+    Year: "1994",
+    City: "Karur"
+}
+
+console.log(personDetails);
